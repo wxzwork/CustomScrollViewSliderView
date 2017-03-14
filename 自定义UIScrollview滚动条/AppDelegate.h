38ab0a9b3c10eba75084328dc7f9816c2199fe3d
WXZScarlett <wxzscarlett@gmail.com>
2017-03-14 14:17:22 +0800

    Initial Commit

---
+++ 自定义UIScrollview滚动条/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  自定义UIScrollview滚动条
+//
+//  Created by WOSHIPM on 2017/3/14.
+//  Copyright © 2017年 WOSHIPM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 自定义UIScrollview滚动条/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  自定义UIScrollview滚动条
+//
+//  Created by WOSHIPM on 2017/3/14.
+//  Copyright © 2017年 WOSHIPM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
